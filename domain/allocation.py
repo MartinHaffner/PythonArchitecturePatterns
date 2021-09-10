245f34085080b35b7a7e8bff38b68734e1f6cd81
Martin Haffner <Martin.Haffner@kybeidos.de>
2021-09-10 22:39:32 +0200

    additional business logic

--- domain/allocation.py
+++ domain/allocation.py
@@ -1,9 +1,26 @@
+from typing import Dict
+
 from .batch import Batch
 from .order import OrderLine
 
 
 class Allocation:
-    @staticmethod
-    def allocate_order_to_batch(batch: Batch, order_line: OrderLine) -> Batch:
+    allocated: Dict[str, str]
+
+    def __init__(self) -> None:
+        self.allocated = {}
+
+    def allocate_order_to_batch(self, batch: Batch, order_line: OrderLine) -> Batch:
+        if batch.sku != order_line.sku:
+            raise ValueError("Batch SKU does not match OrderLine SKU!")
+        if batch.number < order_line.number:
+            raise ValueError("Batch number smaller than OrderLine number!")
+
+        if order_line.order_id in self.allocated.keys():
+            if self.allocated[order_line.order_id] == batch.reference:
+                return batch  # already allocated to the same batch, ignore
+            else:
+                raise ValueError("Order already allocated to another batch")
         result = Batch(batch.reference, batch.sku, batch.number - order_line.number)
+        self.allocated[order_line.order_id] = batch.reference
         return result
--- tests/test_allocation.py
+++ tests/test_allocation.py
@@ -1,3 +1,4 @@
+import pytest
 from domain.allocation import Allocation
 from domain.batch import Batch
 from domain.order import OrderLine
@@ -6,7 +7,47 @@ from domain.order import OrderLine
 def test_allocate_order_to_batch():
     order_line = OrderLine(order_id="12345", sku="RED-CHAIR", number=2)
     batch = Batch(reference="b123", sku="RED-CHAIR", number=20)
-    result = Allocation.allocate_order_to_batch(batch=batch, order_line=order_line)
+    result = Allocation().allocate_order_to_batch(batch=batch, order_line=order_line)
     assert result.reference == batch.reference
     assert result.sku == batch.sku
     assert result.number == 18
+
+
+def test_allocate_order_to_batch_mismatching_sku():
+    order_line = OrderLine(order_id="12345", sku="RED-CHAIR", number=2)
+    batch = Batch(reference="b123", sku="BLUE-CHAIR", number=20)
+    with pytest.raises(ValueError):
+        _ = Allocation().allocate_order_to_batch(batch=batch, order_line=order_line)
+
+
+def test_allocate_order_to_batch_insuffcient_number():
+    order_line = OrderLine(order_id="12345", sku="RED-CHAIR", number=25)
+    batch = Batch(reference="b123", sku="RED-CHAIR", number=20)
+    with pytest.raises(ValueError):
+        _ = Allocation().allocate_order_to_batch(batch=batch, order_line=order_line)
+
+
+def test_allocate_order_to_batch_twice():
+    order_line = OrderLine(order_id="12345", sku="RED-CHAIR", number=2)
+    batch = Batch(reference="b123", sku="RED-CHAIR", number=20)
+    result = Allocation().allocate_order_to_batch(batch=batch, order_line=order_line)
+    assert result.reference == batch.reference
+    assert result.sku == batch.sku
+    assert result.number == 18
+    result = Allocation().allocate_order_to_batch(batch=batch, order_line=order_line)
+    assert result.reference == batch.reference
+    assert result.sku == batch.sku
+    assert result.number == 18
+
+
+def test_allocate_order_to_different_batches():
+    order_line = OrderLine(order_id="12345", sku="RED-CHAIR", number=2)
+    batch_1 = Batch(reference="b123", sku="RED-CHAIR", number=20)
+    batch_2 = Batch(reference="b234", sku="RED-CHAIR", number=20)
+    alloc = Allocation()
+    result = alloc.allocate_order_to_batch(batch=batch_1, order_line=order_line)
+    assert result.reference == batch_1.reference
+    assert result.sku == batch_1.sku
+    assert result.number == 18
+    with pytest.raises(ValueError):
+        _ = alloc.allocate_order_to_batch(batch=batch_2, order_line=order_line)
