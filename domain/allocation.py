edf48dce9b2a2736951f42b7e7c17e780de5f7ad
Martin Haffner <Martin.Haffner@kybeidos.de>
2021-09-10 22:18:11 +0200

    added basic domain logic

--- domain/allocation.py
+++ domain/allocation.py
@@ -5,5 +5,5 @@ from .order import OrderLine
 class Allocation:
     @staticmethod
     def allocate_order_to_batch(batch: Batch, order_line: OrderLine) -> Batch:
-        result = Batch(batch.reference, batch.sku, batch.number - 2)
+        result = Batch(batch.reference, batch.sku, batch.number - order_line.number)
         return result
