ac05fbc8588a51825535399d30ac88f8251e6a68
Martin Haffner <Martin.Haffner@kybeidos.de>
2021-09-11 11:29:54 +0200

    refactoring

--- domain/batch.py
+++ domain/batch.py
@@ -1,8 +1,33 @@
-from dataclasses import dataclass
+from typing import Dict
+
+from .order import OrderLine
 
 
-@dataclass(frozen=True)
 class Batch:
     reference: str
     sku: str
-    number: int
+    initial_size: int
+    remaining_size: int
+    allocated: Dict[str, str]
+
+    def __init__(self, reference: str, sku: str, initial_size: int):
+        self.reference = reference
+        self.sku = sku
+        self.initial_size = initial_size
+        self.remaining_size = initial_size
+        self.allocated = {}
+
+    def can_allocate(self, order_line: OrderLine) -> bool:
+        if self.sku != order_line.sku:
+            return False
+        if self.remaining_size < order_line.number:
+            return False
+        if order_line.order_id in self.allocated.keys():
+            return False
+        return True
+
+    def allocate_order_to_batch(self, order_line: OrderLine) -> None:
+        if not self.can_allocate(order_line):
+            raise ValueError("Cannot allocate this order_line to this batch")
+        self.remaining_size -= order_line.number
+        self.allocated[order_line.order_id] = self.reference
--- tests/test_batch.py
+++ tests/test_batch.py
@@ -1,8 +1,55 @@
+import pytest
 from domain.batch import Batch
+from domain.order import OrderLine
 
 
 def test_create_batch():
-    batch = Batch(reference="b123", sku="RED-CHAIR", number=20)
+    batch = Batch(reference="b123", sku="RED-CHAIR", initial_size=20)
     assert batch.reference == "b123"
     assert batch.sku == "RED-CHAIR"
-    assert batch.number == 20
+    assert batch.initial_size == 20
+    assert batch.remaining_size == 20
+
+
+def _make_same_sku_items(sku, order_size, batch_size):
+    order_line = OrderLine(order_id="o123", sku=sku, number=order_size)
+    batch = Batch(reference="b123", sku=sku, initial_size=batch_size)
+    return order_line, batch
+
+
+def test_allocate_order_to_batch():
+    order_line, batch = _make_same_sku_items("RED-CHAIR", 2, 20)
+    assert batch.can_allocate(order_line) is True
+    batch.allocate_order_to_batch(order_line=order_line)
+    assert batch.reference == batch.reference
+    assert batch.sku == batch.sku
+    assert batch.initial_size == 20
+    assert batch.remaining_size == 18
+
+
+def test_allocate_order_to_batch_mismatching_sku():
+    order_line = OrderLine(order_id="12345", sku="RED-CHAIR", number=2)
+    batch = Batch(reference="b123", sku="BLUE-CHAIR", initial_size=20)
+    assert batch.can_allocate(order_line) is False
+    with pytest.raises(ValueError):
+        batch.allocate_order_to_batch(order_line=order_line)
+
+
+def test_allocate_order_to_batch_insuffcient_number():
+    order_line, batch = _make_same_sku_items("RED-CHAIR", 25, 20)
+    assert batch.can_allocate(order_line) is False
+    with pytest.raises(ValueError):
+        batch.allocate_order_to_batch(order_line=order_line)
+
+
+def test_allocate_order_to_batch_twice():
+    order_line, batch = _make_same_sku_items("RED-CHAIR", 2, 20)
+    assert batch.can_allocate(order_line) is True
+    batch.allocate_order_to_batch(order_line=order_line)
+    assert batch.reference == batch.reference
+    assert batch.sku == batch.sku
+    assert batch.remaining_size == 18
+    assert batch.initial_size == 20
+    assert batch.can_allocate(order_line) is False
+    with pytest.raises(ValueError):
+        batch.allocate_order_to_batch(order_line=order_line)
